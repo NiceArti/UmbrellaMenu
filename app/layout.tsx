8c8a616a061ae434a37121041effaf46d74febd7
NiceArti <nice_arti@mail.ru>
2023-12-03 02:03:14 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Advent_Pro } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+
+// Subsets are really important. CHECK BELOW FOR MORE INFO
+const adventPro = Advent_Pro({ 
+  subsets: ['latin'],
+  style: 'normal',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${adventPro.className}`}>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,207 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Hookah from './Hookah/Hookah';
+import styles from './Layout.module.scss';
+import MenuPosition from './MenuPosition/MenuPosition';
+import MenuPositionsByHeader from './MenuPosition/MenuPositionsByHeader';
+import MenuPositionsWithOnePrice from './MenuPosition/MenuPositionsWithOnePrice';
+import stoneImg from './img/stone-bg.png';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className={`${styles['layout']}`}>
+        <Header />
+        <Hookah />
+        <MenuPosition tag='coffeTag' header='Кофе' positions={[
+            "Капучино",
+            "Латте",
+            "Американо",
+            "Эспрессо",
+            "Какао",
+            "Капучино с маршмеллоу",
+            "Раф-кофе",
+          ]}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          pricing={[
+            170,
+            180,
+            140,
+            120,
+            250,
+            250,
+            220,
+          ]}
         />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <MenuPositionsWithOnePrice tag='' header='Ratte' positions={[
+            "Вафля попкорн",
+            "Малина кокос",
+            "Банановое мороженое с солёной карамелью ",
+            "Сырный",
+          ]}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          pricing={270}
+        />
+        <MenuPositionsWithOnePrice tag='' header='Matcha Latte' positions={[
+            "Классический",
+            "Зелёный бергамот",
+          ]}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          pricing={250}
+        />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+
+        <MenuPositionsByHeader 
+          header='Глинтвейн'
+          pricing={350}
+        />
+
+        <MenuPosition tag='' header='Добавки к чаю и кофе' positions={[
+            "Сироп",
+            "Сливки",
+            "Мёд",
+            "Мята",
+            "Лимон",
+          ]}
+
+          pricing={[
+            30,
+            30,
+            100,
+            30,
+            70,
+          ]}
+        />
+
+        <MenuPositionsWithOnePrice tag='lemonadeTag' header='Лимонады' positions={[
+            "Малина - базилик",
+            "Манго - маракуйя",
+          ]}
+
+          pricing={350}
+        />
+
+
+        <MenuPosition tag='teaTag' header='Китайский чай' positions={[
+            "Те Гуанинь",
+            "ГАБА",
+            "Да Хун Пао",
+            "Шу Пуэр",
+            "Шен Пуэр",
+            "Улун Дыня",
+            "Хуа ЧжуЧа (жасмин)",
+            "Бай Му Дань (Белый пион)",
+            "Молочный улун",
+          ]}
+
+          pricing={[
+            550,
+            700,
+            450,
+            500,
+            450,
+            400,
+            400,
+            450,
+            500,
+          ]}
+        />
+
+        <MenuPosition tag='' header='Согревающие чайные напитки' positions={[
+            "Марокканский чай",
+            "Грушевый чай с жасмином",
+            "Ягодный чай",
+            "Облепиховый чай с имбирем",
+            "Брусника-клюква",
+          ]}
+
+          pricing={[
+            400,
+            550,
+            450,
+            500,
+            550,
+          ]}
+        />
+
+
+        <MenuPositionsWithOnePrice tag='' header='Чай чёрный' positions={[
+            "Английский завтрак",
+            "Эрл Грей",
+            "Липовый мёд",
+            "Манго – Маракуйя",
+            "Яблоко – Корица",
+            "Ноктюрн",
+            "Чабрец",
+            "Земляничный десерт",
+          ]}
+
+          pricing={350}
+        />
+
+
+        <MenuPositionsWithOnePrice tag='' header='Чай зелёный' positions={[
+            "Японская липа",
+            "Лимонный крем с женьшенем",
+            "Свежая ромашка",
+            "Сенча",
+            "Сокровище Японии",
+            "Романтика",
+            "Сладкая груша",
+          ]}
+
+          pricing={350}
+        />
+
+        <MenuPositionsWithOnePrice tag='' header='Чайные напитки' positions={[
+            "Вот фрукт!",
+            "Тонус",
+            "Айболит",
+            "Освежающий",
+          ]}
+
+          pricing={350}
+        />
+        <MenuPositionsWithOnePrice tag='milkshakeTag' header='Милкшейки' positions={[
+            "Oreo",
+            "Банановый",
+            "Груша-дыня",
+            "Фисташковый",
+          ]}
+
+          pricing={350}
+        />
+
+
+        <MenuPosition tag='snacksTag' header='Снеки' positions={[
+            "Оreo",
+            "Choco-pie",
+            "Lays STAX",
+          ]}
+
+          pricing={[
+            100,
+            100,
+            300,
+          ]}
+        />
+
+        <MenuPositionsWithOnePrice tag='beerTag' header='Пиво' positions={[
+            "Белый кролик",
+            "Два Бобра",
+            "Груша-дыня",
+            "Червонный валет",
+          ]}
+
+          pricing={250}
+        />
+
+        <Footer />
+
+        <Image className={styles["bg-img-1"]} src={stoneImg} alt="stone"/>
+        <Image className={styles["bg-img-2"]} src={stoneImg} alt="stone"/>
       </div>
     </main>
   )
