823b7b645e25b8a8de92d3cc389af6eac9a91d9a
Nice Arti <nice_arti@mail.ru>
2025-02-20 14:53:01 +0200

    fix: padding

--- widgets/menu-positions.tsx
+++ widgets/menu-positions.tsx
@@ -59,10 +59,10 @@ const PositionsTableView = ({names, prices}:{names: string[], prices: string[]})
             <tbody>
                 {names.map((name, index) => (
                     <tr key={index} className="inline-flex justify-between w-full text-xl font-semibold tracking-wider">
-                        <td className="grow max-w-[304px] py-2">
+                        <td className="grow max-w-[304px] py-[6px]">
                             {name}
                         </td>
-                        <td className="border-l-2 border-primary min-w-[80px] w-20 justify-center items-end text-right p-2">
+                        <td className="border-l-2 border-primary min-w-[80px] w-20 justify-center items-end text-right py-[6px]">
                             {index < prices.length ? prices[index] : ""}
                         </td>
                     </tr>
