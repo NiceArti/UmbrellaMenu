2b22aeb3dd6ba08b4cd2b1c2ec12dd6e1206dfd0
Nice Arti <nice_arti@mail.ru>
2025-09-12 21:11:47 +0300

    feat: update constants | gitignore

--- shared/constants/constants.ts
+++ shared/constants/constants.ts
@@ -1,4 +1,4 @@
 export const ADMIN_LOGIN = process.env.ADMIN_LOGIN;
 export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 export const ADMIN_COOKIE_NAME = "admin_auth";
-export const ADMIN_COOKIE_MAX_AGE = 60 * 60 * 24 * 10; // 10 days in seconds
+export const ADMIN_COOKIE_MAX_AGE = 60 * 60 * 24 * 30; // 30 days in seconds
